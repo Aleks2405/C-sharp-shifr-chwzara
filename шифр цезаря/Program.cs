17ac3c27521b490274797a87f57b130392b3397a
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2022-01-26 17:38:21 +0300

    как что так но еще не финал

--- шифр цезаря/Program.cs
+++ шифр цезаря/Program.cs
@@ -18,77 +18,28 @@ namespace шифр_цезаря
             Console.WriteLine("Введите строку которую нужно зашифровать ");
             string a = Console.ReadLine();
             char[] arr = a.ToCharArray();
+            int d;
             for (int i = 0; i < a.Length; i++)
             {
 
                 //arr[i] = (char)(arr[i] + 3);
-
-                arr[i] = (char)arr[i];
+               // arr[i] = (char)arr[i];
                 if ('a' <= arr[i] || arr[i] >= 'z')
                 {
-                    arr[i] = (char)(arr[i] + 3);
-                    if (arr[i] > 'w' + 3)
+                    // arr[i] = (char)(arr[i] + 3);
+                    if (arr[i]>'z')
                     {
-                        Console.WriteLine("tyt");
-
-
-                        if (arr[i] > 'w' + 3)
-                        {
-                            arr[i] = 'a';
-                            arr[i]++;
-
-                            // arr[i] = (char)(arr[i] + 1);
-                            // arr[i]++;
-                            //break;
-                        }
+                    
+                        arr[i] = (char) (arr[i] - 'z');
                     }
+                    arr[i] = (char)(arr[i] + 3);
                 }
-
-                //switch((int) arr[i])
-                //{
-                //    case ((int)'x' + 3):
-                //        arr[i] = 'a';
-                //        break;
-                //    case ((int)'y' + 3):
-                //        arr[i] = 'b';
-                //        break;
-                //    case ((int)'z' + 3):
-                //        arr[i] = 'c';
-                //        break;
-                //    case ((int)'X'+3):
-                //        arr[i] = 'A';
-                //        break;
-                //    case ((int)'Y' + 3):
-                //        arr[i] = 'B';
-                //        break;
-                //    case ((int)'Z' + 3):
-                //        arr[i] = 'C';
-                //        break;
-                //}
-                //Console.WriteLine((int)(arr[i]));
                 a = new string(arr);
             }
-            Console.WriteLine(a); // убедились что работает 
-
-            //for (int i = 0; i < arr.Length; i++)
-            //{
-            //   arr[i] = (char)(arr[i] - 3);
-            //    Console.WriteLine(arr[i]);
-            //}
-            // a = new string(arr);
-            // Console.WriteLine();
-            // Console.WriteLine(a + "yraa");
-            //for (int i = 0; i < a.Length; i++)
-            //{
-            //    if (arr[i] >= 'a' && arr[i] <= 'z' || arr[i] >= 'а' && arr[i] <= 'я')
-            //    {
-            //        Console.Write((char)(arr[i] - 3));
-            //    }
-
-            //}
-
+            Console.WriteLine(a);
         }
     }
 }
+
 
 
