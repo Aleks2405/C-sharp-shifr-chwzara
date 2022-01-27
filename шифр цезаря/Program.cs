3c3d49bbf4a92d648f7a2c800debeded84424822
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2022-01-27 17:13:22 +0300

    заработал шифр маленький регистр

--- шифр цезаря/Program.cs
+++ шифр цезаря/Program.cs
@@ -27,16 +27,51 @@ namespace шифр_цезаря
                 if ('a' <= arr[i] || arr[i] >= 'z')
                 {
                     // arr[i] = (char)(arr[i] + 3);
-                    if (arr[i]>'z')
+                    if (arr[i]>'w')
                     {
-                    
-                        arr[i] = (char) (arr[i] - 'z');
+                        if((char) arr[i] == 'x')
+                        {
+                            arr[i] = (char)('a' - 3);
+
+                        } else if ((char)arr[i] == 'y')           /* возможно слишком уж не красиво но придумал так был вариант добавить алфавит вручную но что так что так не красиво*/
+                        {
+                            arr[i] = (char)('b' - 3);
+                        }
+                        else if ((char)arr[i] == 'z')
+                        {
+                           arr[i] = (char)('c' - 3);
+                        }
+                        //arr[i] = (char) (arr[i] - 'z');
                     }
                     arr[i] = (char)(arr[i] + 3);
                 }
                 a = new string(arr);
             }
             Console.WriteLine(a);
+            char[] arr1 = a.ToCharArray();
+            for (int i = 0; i < a.Length; i++)
+            {
+                if ('a' <= arr[i] || arr[i] >= 'z')
+                {
+                    if ((char)arr[i] == 'a')
+                    {
+                        arr[i] = (char)('x' + 3);
+
+                    }
+                    else if ((char)arr[i] == 'b')           /* возможно слишком уж не красиво но придумал так был вариант добавить алфавит вручную но что так что так не красиво*/
+                    {
+                        arr[i] = (char)('y' + 3);
+                    }
+                    else if ((char)arr[i] == 'c')
+                    {
+                        arr[i] = (char)('z' + 3);
+                    }
+                    arr[i] = (char)(arr[i] - 3);
+                }
+                a = new string(arr);
+            }
+            Console.WriteLine(a + " Дешифровка");
+
         }
     }
 }
